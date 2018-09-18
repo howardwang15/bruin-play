d5926976be929ea95cc31a9b55e97ab46df00543
howardwang15 <joy_luck_2000@hotmail.com>
2018-09-17 22:06:50 -0700

    sent user object back to frontend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.post('/login', (req, res) => {
     req.session.user = req.body.user;
-    res.sendStatus(200);
+    res.status(200).send(req.body.user);
 });
 
 module.exports = router;
